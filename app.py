86e965d302390671e6df2c503180657a511098e6
1114514 <62145553+1114514@users.noreply.github.com>
2020-09-05 20:09:16 +0800

    Update app.py
    csrf使用正则匹配，不用手动填写
    更改了一点格式

--- app.py
+++ app.py
@@ -10,20 +10,23 @@ import asyncio
 import aiohttp
 import json
 import random
+import re
 
-oid=114514 # av号
-ua='Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.142 Safari/537.36' # 改成你自己的ua, 不改应该问题也不大
-cookie='' # 填写你自己的cookie, 格式: 'a=b; c=d'
-csrf='' # cookie中的bili_jct字段
-get_url=f'https://api.bilibili.com/x/v2/reply?&type=1&pn=1&oid={oid}'
-del_url='https://api.bilibili.com/x/v2/reply/del'
+oid = 114514 # 自己视频的av号
+ua = 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.142 Safari/537.36' # 改成你自己的ua, 不改应该问题也不大
+cookie = '' # 填写你自己的cookie, 格式: 'a=b; c=d'
+
+get_url = f'https://api.bilibili.com/x/v2/reply?&type=1&pn=1&oid={oid}'
+del_url = 'https://api.bilibili.com/x/v2/reply/del'
+reobj = re.compile('bili_jct=(.*?); ')
+csrf = reobj.match(cookie).group(1)
 
 async def getCommentData(oid):
     global ua
-    headers={
+    headers = {
         'User-Agent':ua,
     }
-    async with aiohttp.ClientSession(headers=headers) as session
+    async with aiohttp.ClientSession(headers=headers) as session:
         async with session.get(get_url) as resp:
             text = await resp.text()
             dic = json.loads(text)
@@ -34,12 +37,12 @@ async def getCommentData(oid):
 async def delComment(oid,rpid):
     global ua,cookie,csrf
     print('will delete rpid:',rpid)
-    headers={
+    headers = {
         'User-Agent':ua,
         'Cookie':cookie
     }
     async with aiohttp.ClientSession(headers=headers) as session:
-        postData={
+        postData = {
             'oid': oid,
             'type': 1,
             'jsonp': 'jsonp',
@@ -56,8 +59,8 @@ async def main():
     while True:
         await asyncio.sleep(4.5+random.random())
         count , data = await getCommentData(oid)
-        if count > c :
-            delCount = count - c
+        if count>c :
+            delCount = count-c
             tasks = []
             for comment in data['data']['replies'][:delCount]:
                 task = delComment(oid,comment['rpid'])
